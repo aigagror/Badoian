fbea3e7c3ff3e2d1a108c79c5494838407452205
Eddie Huang <aigagror@gmail.com>
2018-05-09 23:39:07 -0500

    Google Oauth works

--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect
+from django.template import Context, Template
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
 def index(request):
+    if not request.user.is_authenticated:
+        url = Template("{% url 'social:begin' 'google-oauth2' %}")
+        return redirect(str(url.render(Context({}))))
+
     return render(request, template_name='index.html')
+
+def logout(request):
+    auth_logout(request)
+    return render(request, 'logout.html')
 
