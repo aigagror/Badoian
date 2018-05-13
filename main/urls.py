0f4acd7ebbf632be5c8ce60f666e4f4cd74af4a5
Eddie Huang <aigagror@gmail.com>
2018-05-13 15:17:02 -0500

    Statistics page

--- main/urls.py
+++ main/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('assignments/', views.assignments, name='assignments'),
+    path('statistics/', views.statistics, name='statistics'),
     path('login/', views.login, name='login'),
     path('logout/', views.logout, name='logout'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render, redirect
 from django.template import Context, Template
 from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.decorators import *
 
 # Create your views here.
+@login_required
 def index(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-
     return render(request, template_name='index.html')
 
+@login_required
 def assignments(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-
     return render(request, template_name='assignments.html')
+
+@login_required
+def statistics(request):
+    return render(request, template_name='statistics.html')
 
 def login(request):
     return render(request, 'login.html')
