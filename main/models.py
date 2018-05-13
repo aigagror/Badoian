38e9c3463c642f67933844482dc454360b474c5d
Eddie Huang <aigagror@gmail.com>
2018-05-13 11:07:55 -0500

    More model stuff

--- main/models.py
+++ main/models.py
@@ -7,24 +7,12 @@ from django.utils import timezone
 class Problem(models.Model):
     question = models.TextField()
 
-    answer = models.CharField(max_length=256)
+    solution = models.CharField(max_length=256)
 
 class Round(models.Model):
     problems = models.ManyToManyField(Problem, related_name='rounds')
 
     image = models.ImageField(null=True, blank=True)
-
-
-class Submission(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
-
-    time = models.DateTimeField(default=timezone.now)
-
-    submission = models.CharField(max_length=256)
-
-    submission_image = models.ImageField(null=True, blank=True)
 
 
 class Team(models.Model):
@@ -35,3 +23,20 @@ class Team(models.Model):
     coaches = models.ManyToManyField(User, related_name='coach_teams')
 
     head_coaches = models.ManyToManyField(User, related_name='head_coach_teams')
+
+
+class Assignment(models.Model):
+    due_time = models.DateTimeField()
+
+    round = models.ForeignKey(Round, on_delete=models.CASCADE)
+
+class Submission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+
+    time = models.DateTimeField(default=timezone.now)
+
+    answers = models.TextField(default="") # Each answer is in its own line
+
+    submission_image = models.ImageField(null=True, blank=True)
