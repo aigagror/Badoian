af6f30a240268320a28f3a8bf3fec44e1276e22f
Eddie Huang <aigagror@gmail.com>
2018-05-13 10:53:01 -0500

    Timezone. EditedModels

--- badoian/settings.py
+++ badoian/settings.py
@@ -140,8 +140,6 @@ SOCIAL_AUTH_STORAGE = 'social_django.models.DjangoStorage'
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
-
 USE_I18N = True
 
 USE_L10N = True
--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
@@ -9,9 +10,9 @@ class Problem(models.Model):
     answer = models.CharField(max_length=256)
 
 class Round(models.Model):
-    problems = models.ManyToManyField(Problem, related_name='Round')
+    problems = models.ManyToManyField(Problem, related_name='rounds')
 
-    image = models.ImageField()
+    image = models.ImageField(null=True, blank=True)
 
 
 class Submission(models.Model):
@@ -19,10 +20,18 @@ class Submission(models.Model):
 
     problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
 
+    time = models.DateTimeField(default=timezone.now)
+
     submission = models.CharField(max_length=256)
+
+    submission_image = models.ImageField(null=True, blank=True)
 
 
 class Team(models.Model):
     name = models.CharField(max_length=32)
 
-    members = models.ManyToManyField(User, related_name='Team')
+    members = models.ManyToManyField(User, related_name='teams')
+
+    coaches = models.ManyToManyField(User, related_name='coach_teams')
+
+    head_coaches = models.ManyToManyField(User, related_name='head_coach_teams')
