bc95cfc4dabb07e6fa5ae17d97a76de02c3a58fd
Eddie Huang <aigagror@gmail.com>
2018-05-13 15:54:58 -0500

    modal for new assignemnt. backend for creating and joining teams

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.template import Context, Template
 from django.contrib.auth import logout as auth_logout
 from django.contrib.auth.decorators import *
+from .models import *
 
 # Create your views here.
 @login_required
@@ -27,3 +28,26 @@ def logout(request):
     auth_logout(request)
     return render(request, 'logout.html')
 
+@login_required
+def create_team(request):
+    if request.method == 'POST':
+        name = request.POST.get('team_name')
+
+        new_team = Team(name=name)
+
+        new_team.head_coaches = request.user
+
+        new_team.save()
+
+        return redirect('index')
+
+@login_required
+def join_team(request):
+    if request.method == 'POST':
+        team_id = int(request.POST.get('team_id'))
+
+        team = Team.objects.get(id=team_id)
+
+        team.members.add(request.user)
+
+        team.save()
