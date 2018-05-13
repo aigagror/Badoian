b52edf1cebda3ee5285ba7296a078d5d859d1ef4
Eddie Huang <aigagror@gmail.com>
2018-05-12 20:50:55 -0500

    Round and submisison models

--- main/models.py
+++ main/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,3 +7,16 @@ class Problem(models.Model):
     question = models.TextField()
 
     answer = models.CharField(max_length=256)
+
+class Round(models.Model):
+    problems = models.ManyToManyField(Problem, related_name='Round')
+
+    image = models.ImageField()
+
+
+class Submission(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    problem = models.ForeignKey(Problem, on_delete=models.CASCADE)
+
+    submission = models.CharField(max_length=256)
